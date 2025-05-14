cfe41cc2334f446057b6d862373af3ba34972075
zsuffa david <zsuffa.david@gmail.com>
2025-05-14 22:27:59 +0200

    basic location setting getting

--- src/components/app-shell.js
+++ src/components/app-shell.js
@@ -1,4 +1,5 @@
-import {html, css} from 'lit';
+import {html, css, render} from 'lit';
+// import {classMap} from 'lit/directives/class-map.js';
 import WeatherLitElement from './weather-lit-element';
 import {BasicWeatherWidget} from './basic-weather-widget.js';
 import {DailyForecastWidget} from './daily-forecast-widget.js';
@@ -10,6 +11,7 @@ export class AppShell extends WeatherLitElement {
             ...super.properties,
 
             loading: {type: Boolean},
+            currentLocation: {type: Object},
             latitude: {type: Number},
             longitude: {type: Number},
             weatherData: {type: Object},
@@ -19,8 +21,13 @@ export class AppShell extends WeatherLitElement {
 
     constructor() {
         super();
-        this.latitude = JSON.parse(localStorage.getItem('latitude')) || -90;
-        this.longitude = JSON.parse(localStorage.getItem('longitude')) || -45;
+        this.currentLocation = JSON.parse(localStorage.getItem('currentLocation')) || {
+            country: undefined,
+            elevation: undefined,
+            latitude: undefined,
+            longitude: undefined,
+            city: undefined,
+        };
 
         this.dailyForecastData = [];
         this.weatherData = {};
@@ -38,27 +45,37 @@ export class AppShell extends WeatherLitElement {
         super.connectedCallback();
 
         // Load font using FontFace API
-    const font = new FontFace(
-        'weathericons',
-        'url(/font/weathericons-regular-webfont.woff2) format("woff2")'
-      );
+        const font = new FontFace('weathericons', 'url(/font/weathericons-regular-webfont.woff2) format("woff2")');
 
-      font.load().then(loadedFont => {
-        // Add font to document's FontFaceSet
-        document.fonts.add(loadedFont);
-        console.log('Font loaded successfully in parent!');
+        font.load()
+            .then((loadedFont) => {
+                // Add font to document's FontFaceSet
+                document.fonts.add(loadedFont);
+                console.log('Font loaded successfully in parent!');
 
-        // Dispatch an event to notify children that font is loaded
-        this.dispatchEvent(new CustomEvent('font-loaded', {
-          bubbles: true,
-          composed: true,
-          detail: { fontFamily: 'weathericons' }
-        }));
+                // Dispatch an event to notify children that font is loaded
+                this.dispatchEvent(
+                    new CustomEvent('font-loaded', {
+                        bubbles: true,
+                        composed: true,
+                        detail: {fontFamily: 'weathericons'},
+                    }),
+                );
 
-        this.requestUpdate();
-      }).catch(error => {
-        console.error('Failed to load font:', error);
-      });
+                this.requestUpdate();
+            })
+            .catch((error) => {
+                console.error('Failed to load font:', error);
+            });
+    }
+
+    async updated(_changedProperties) {
+        if (_changedProperties.has('currentLocation')) {
+            console.log('[updated] this.currentLocation', this.currentLocation);
+            this.fetchAndUpdateWeather();
+        }
+
+        super.updated(_changedProperties);
     }
 
     async firstUpdated() {
@@ -76,36 +93,43 @@ export class AppShell extends WeatherLitElement {
 
     async fetchWeatherData() {
         // Graz
-        const latitude = this.latitude;
-        const longitude = this.longitude;
+        const latitude = this.currentLocation.latitude;
+        const longitude = this.currentLocation.longitude;
 
-        const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode&current_weather=true&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=auto`;
+        if (latitude && longitude) {
+            const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode&current_weather=true&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=auto`;
 
-        try {
-            const response = await fetch(url);
-            if (!response.ok) throw new Error('API request failed');
-            return response.json();
-        } catch (error) {
-            console.log('Error fetching weather data:', error);
+            try {
+                const response = await fetch(url);
+                if (!response.ok) throw new Error('API request failed');
+                return response.json();
+            } catch (error) {
+                console.log('Error fetching weather data:', error);
+            }
         }
     }
 
     getLocation() {
-        if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                console.log('latitude', position.coords.latitude, 'longitude', position.coords.longitude);
-                this.latitude = position.coords.latitude;
-                this.longitude = position.coords.longitude;
-                // Store data to localStorage
-                localStorage.setItem('latitude', JSON.stringify(this.latitude));
-                localStorage.setItem('longitude', JSON.stringify(this.longitude));
+        if ('geolocation' in navigator) {
+            try {
+                navigator.geolocation.getCurrentPosition((position) => {
+                    console.log('position', position);
+                    this.currentLocation = {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                    };
+                    // Store data to localStorage
+                    localStorage.setItem('currentLocation', JSON.stringify(this.currentLocation));
 
-                this.fetchAndUpdateWeather();
-            });
-          } else {
+                    this.fetchAndUpdateWeather();
+                });
+            } catch (e) {
+                console.log(e);
+            }
+        } else {
             alert("Your browser doesn't support geolocation. Enter your location city manually");
             return false;
-          }
+        }
     }
 
     async fetchGeocodingApi(event) {
@@ -116,7 +140,7 @@ export class AppShell extends WeatherLitElement {
             let cityCharacters = event.target.value;
             const url = `https://geocoding-api.open-meteo.com/v1/search?name=${cityCharacters}&count=10&language=en&format=json`;
             const options = {
-                method: 'GET'
+                method: 'GET',
             };
             const response = await fetch(url, options);
             const geoData = await response.json();
@@ -126,13 +150,12 @@ export class AppShell extends WeatherLitElement {
                 geoData.results.forEach((location) => {
                     console.log(`location`, location);
                     locationList.push(html`
-                        <div class="location" @click="${(e) => {
-                                this.selectLocation(e);
-                            }}"
-                            data-longitude=${location.longitude}
-                            data-latitude=${location.latitude}
-                            data-city=${location.name}>
-                            ${location.name} [${location.country_code}]
+                        <div
+                            class="location"
+                            @click="${() => {
+                                this.selectLocation(location);
+                            }}">
+                            ${location.name} [${location.country}]
                         </div>
                     `);
                 });
@@ -140,17 +163,22 @@ export class AppShell extends WeatherLitElement {
                 resultsList.classList.add('enabled');
                 render(locationList, resultsList);
             }
-        } catch(e) {
+        } catch (e) {
             console.log('Geocoding API fetch failed: ', e);
         }
     }
 
-    selectLocation(event) {
-        console.log('event', event);
-        console.log('event.target', event.target);
-        console.log(event.target.dataset.latitude);
-        console.log(event.target.dataset.longitude);
-        console.log(event.target.dataset.city);
+    selectLocation(location) {
+        console.log('location', location);
+        this.currentLocation = {
+            country: location.country,
+            elevation: location.elevation,
+            latitude: location.latitude,
+            longitude: location.longitude,
+            city: location.name,
+        };
+
+        localStorage.setItem('currentLocation', JSON.stringify(this.currentLocation));
 
         const resultsList = this._('.location-results');
         resultsList.classList.remove('enabled');
@@ -160,12 +188,17 @@ export class AppShell extends WeatherLitElement {
         return html`
             <div class="add-city">
                 <input
+                    id="city-search-input"
+                    class="city-search-input"
                     type="text"
-                    @input="${this.fetchGeocodingApi}"
-                    placeholder="Enter City Name"/>
+                    @input="${(event) => {
+                        if (event.target.value.length >= 2) {
+                            this.fetchGeocodingApi(event);
+                        }
+                    }}"
+                    placeholder="Enter City Name" />
 
-                <div class="location-results">
-                </div>
+                <div class="location-results"></div>
             </div>
         `;
     }
@@ -174,8 +207,8 @@ export class AppShell extends WeatherLitElement {
         return html`
             <div class="location-settings">
                 <button @click="${this.getLocation}">Get Location</button>
-                ${this.latitude.toFixed(2)}
-                ${this.longitude.toFixed(2)}
+                ${this.currentLocation.latitude ? this.currentLocation.latitude.toFixed(2) : ''}
+                ${this.currentLocation.longitude ? this.currentLocation.longitude.toFixed(2) : ''}
             </div>
         `;
     }
@@ -193,7 +226,7 @@ export class AppShell extends WeatherLitElement {
                 background-color: #f3f3f3;
                 color: black;
                 padding: 1em;
-                border-radius: .25em;
+                border-radius: 0.25em;
                 margin-bottom: 1em;
                 display: flex;
                 justify-content: space-between;
@@ -201,13 +234,14 @@ export class AppShell extends WeatherLitElement {
             main {
                 padding: 1em;
                 background-color: #f5f5f5;
-                border-radius: .25em;
+                border-radius: 0.25em;
             }
             .location-results {
                 display: none;
                 background: white;
                 flex-direction: column;
                 gap: 5px;
+                cursor: pointer;
 
                 &.enabled {
                     display: flex;
@@ -216,7 +250,15 @@ export class AppShell extends WeatherLitElement {
             .location {
                 padding: 9px;
                 &:hover {
-                    background: rgba(0,0,0,0.5);
+                    background: rgba(0, 0, 0, 0.24);
+                }
+            }
+            .current-location {
+                display: flex;
+                flex-direction: column;
+
+                h1 {
+                    margin: 0;
                 }
             }
         `;
@@ -225,9 +267,15 @@ export class AppShell extends WeatherLitElement {
     render() {
         return html`
             <header>
-                <h1>Graz</h1>
-                ${this.renderSetCityWidget()}
-                ${this.renderGetLocationButton()}
+                <div class="current-location">
+                    <h1>${this.currentLocation.city}</h1>
+                    <div class="country">${this.currentLocation.country}</div>
+                    <div class="elevation">
+                        ${this.currentLocation.elevation}
+                        <span class="unit">m</span>
+                    </div>
+                </div>
+                ${this.renderSetCityWidget()} ${this.renderGetLocationButton()}
             </header>
             <main>
                 <basic-weather-widget
